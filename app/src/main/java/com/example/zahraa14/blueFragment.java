23bc4501d3c2443d95185f82782ded2e4ddb325f
zuliana21 <zuli25176@gmail.com>
2022-10-19 08:20:10 +0700

    first commit

---
+++ app/src/main/java/com/example/zahraa14/blueFragment.java
@@ -0,0 +1,6 @@
+package com.example.zahraa14;
+
+import android.app.Activity;
+
+public class blueFragment extends Activity {
+}
---
+++ app/src/main/java/com/example/zahraa14/yellowFragment.java
@@ -0,0 +1,6 @@
+package com.example.zahraa14;
+
+import android.app.Activity;
+
+public class yellowFragment extends Activity {
+}
